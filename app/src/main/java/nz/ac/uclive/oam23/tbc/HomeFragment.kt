ad9506a5bb29ca8c8964b5098dfb864edab370dd
James Kwok <jamesuni2018@gmail.com>
2021-05-03 22:33:17 +1200

    Set up the home fragment to include a map.

--- app/src/main/java/nz/ac/uclive/oam23/tbc/HomeFragment.kt
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/HomeFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +22,27 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
+    /**
+     * Function to change the map once it is created.
+     * Add markers/move camera/set zoom here as required.
+     */
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    /**
+     * Creates the map, calls the OnMapReadyCallback() function.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
