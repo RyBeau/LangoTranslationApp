c28094a27c9b3f51e6145a79b8d1142a01272d8a
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-05-06 16:47:13 +1200

    Adjust layout for landscape

---
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/PreviousTranslationAdapter.kt
@@ -0,0 +1,51 @@
+package nz.ac.uclive.oam23.tbc
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PreviousTranslation(val date: String, val originalText: String){}
+
+class PreviousTranslationAdapter(private var translations: Array<PreviousTranslation>, private val onPreviousTranslationListener: OnPreviousTranslationListener)
+    : RecyclerView.Adapter<PreviousTranslationAdapter.PreviousTranslationViewHolder>() {
+
+    class PreviousTranslationViewHolder(itemView: View, private val onPreviousTranslationListener: OnPreviousTranslationListener)
+        : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        val date: TextView = itemView.findViewById(R.id.date)
+        val originalText: TextView = itemView.findViewById(R.id.originalText)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View?) {
+            onPreviousTranslationListener.onTranslationClick(adapterPosition)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PreviousTranslationViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.previous_translation_item, parent.findViewById(R.id.previousTranslationsViewer), false)
+        return PreviousTranslationViewHolder(view, onPreviousTranslationListener)
+    }
+
+    override fun onBindViewHolder(viewHolder: PreviousTranslationViewHolder, position: Int) {
+        viewHolder.date.text = translations[position].date
+        viewHolder.originalText.text = translations[position].originalText
+    }
+
+    override fun getItemCount() = translations.size
+
+    fun setData(newTranslations: Array<PreviousTranslation>) {
+        translations = newTranslations
+        notifyDataSetChanged()
+    }
+
+
+    interface OnPreviousTranslationListener {
+        fun onTranslationClick(position: Int)
+    }
+}
--- app/src/main/java/nz/ac/uclive/oam23/tbc/StoredTranslationsFragment.kt
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/StoredTranslationsFragment.kt
@@ -5,55 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class StoredTranslationsFragment : Fragment(), PreviousTranslationAdapter.OnPreviousTranslationListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StoredTranslationsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class StoredTranslationsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val tempTranslationsList = arrayOf<PreviousTranslation>(
+        PreviousTranslation("1/1/2021", "Test Text"),
+        PreviousTranslation("2/1/2021", "Test Text"),
+        PreviousTranslation("3/1/2021", "Test Text Longer"),
+        PreviousTranslation("4/1/2021", "Test Text"),
+        PreviousTranslation("5/1/2021", "Test Text"),
+        PreviousTranslation("6/1/2021", "Test Text Longer"),
+        PreviousTranslation("7/1/2021", "Test Text"),
+        PreviousTranslation("8/1/2021", "Test Text"),
+        PreviousTranslation("9/1/2021", "Test Text Longest Text of all"),
+        PreviousTranslation("10/1/2021", "Test Text")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stored_translations, container, false)
+        val view =  inflater.inflate(R.layout.fragment_stored_translations, container, false)
+        val translationAdapter = PreviousTranslationAdapter(tempTranslationsList, this)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.previousTranslationsViewer)
+        recyclerView.apply {
+            adapter = translationAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StoredTranslationsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StoredTranslationsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onTranslationClick(position: Int) {
+        Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show()
     }
 }
