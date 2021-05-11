81c7aceadef77a694f2cfcf9b5cfdf0aba9eb9c2
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-05-11 22:13:28 +1200

    add code for recogniser

--- app/src/main/java/nz/ac/uclive/oam23/tbc/SaveEditTranslationFragment.kt
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/SaveEditTranslationFragment.kt
@@ -5,29 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import java.io.IOException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SaveEditTranslationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SaveEditTranslationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val recogniser = TextRecognition.getClient()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +24,22 @@ class SaveEditTranslationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_save_edit_translation, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SaveEditTranslationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SaveEditTranslationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun detectText(imagePath: String) {
+        val image: InputImage
+        try {
+            image = InputImage.fromFilePath(context, imagePath.toUri())
+            val result = recogniser.process(image)
+                .addOnSuccessListener { visionText -> {
+                    //TODO Set text box value
                 }
-            }
+                }
+                .addOnFailureListener { e -> {
+                    //TODO Handle failure
+                    e.printStackTrace()
+                }
+                }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 }
