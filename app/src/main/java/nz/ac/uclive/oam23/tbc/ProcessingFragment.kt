3250c19fb02a8b1e511914e4002128c1cdeecff0
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-05-17 14:49:01 +1200

    add processing text

--- app/src/main/java/nz/ac/uclive/oam23/tbc/ProcessingFragment.kt
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/ProcessingFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.net.toUri
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import java.io.IOException
 
 class ProcessingFragment : Fragment() {
     val recogniser = TextRecognition.getClient()
+    lateinit var currentAction: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +24,12 @@ class ProcessingFragment : Fragment() {
         val mainActivity = activity as MainActivity
         mainActivity.setLocation(MainActivity.Location.PROCESSING)
         return inflater.inflate(R.layout.fragment_processing, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        currentAction = view.findViewById(R.id.currentAction)
+        currentAction.text = getString(R.string.recognising_text)
     }
 
     private fun detectText(imagePath: String) {
