5179754c62285ccca5e3b40107d7349c1bfb9443
James Kwok <jtk39@uclive.ac.nz>
2021-05-21 22:49:52 +1200

    Set up theme changing via preferences.

--- app/src/main/java/nz/ac/uclive/oam23/tbc/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/MainActivity.kt
@@ -1,23 +1,13 @@
 package nz.ac.uclive.oam23.tbc
 
-import android.Manifest
 import android.app.AlertDialog
-import android.content.Context
-import android.content.SharedPreferences
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.*
-import androidx.core.content.ContextCompat
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.preference.PreferenceManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -60,25 +50,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Sets a theme based on SharedPreferences which are changed in the settings page.
-     */
-     fun setTheme() {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this /* Activity context */)
-        when (sharedPreferences.getString("themes","OS")) {
-            "OS" -> {
-                setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
-            }
-            "Light" -> {
-                setDefaultNightMode(MODE_NIGHT_NO)
-            }
-            "Dark" -> {
-                setDefaultNightMode(MODE_NIGHT_YES)
-            }
-        }
-
-    }
-
-    /**
      * Creates confirmation dialog for exiting the NewRoundFragment
      */
     private fun onBackConfirmation() {
@@ -108,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_preferences, R.id.navigation_previous))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        setTheme()
+        setTheme(this)
     }
 
 }
--- app/src/main/java/nz/ac/uclive/oam23/tbc/PreferencesFragment.kt
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/PreferencesFragment.kt
@@ -1,12 +1,10 @@
 package nz.ac.uclive.oam23.tbc
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 
 /**
@@ -19,6 +17,22 @@ class PreferencesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Loads Shared preferences
+        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        //Setup a shared preference listener for hpwAddress and restart transport
+        val listener: SharedPreferences.OnSharedPreferenceChangeListener =
+                SharedPreferences.OnSharedPreferenceChangeListener { _: SharedPreferences, key: String ->
+                    if (key == "themes") {
+                        setTheme(requireActivity())
+                    }
+                }
+        prefs.registerOnSharedPreferenceChangeListener(listener)
+
+    }
+
 
 
 
