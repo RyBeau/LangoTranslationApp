73a02391e8cd2ddd730aa0c80020595a71bf45bf
Olivia Mackintosh <oam23@uclive.ac.nz>
2021-05-27 15:31:02 +1200

    Created foreground service for location
    Created a foreground service that gets location updates to assess whether the user is near any previous translations.

--- app/src/main/java/nz/ac/uclive/oam23/tbc/PreferencesFragment.kt
+++ app/src/main/java/nz/ac/uclive/oam23/tbc/PreferencesFragment.kt
@@ -1,5 +1,8 @@
 package nz.ac.uclive.oam23.tbc
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -22,9 +25,11 @@ class PreferencesFragment : PreferenceFragmentCompat() {
         //Setup a shared preference listener for hpwAddress and restart transport
         val listener: SharedPreferences.OnSharedPreferenceChangeListener =
                 SharedPreferences.OnSharedPreferenceChangeListener { _: SharedPreferences, key: String ->
-                    if (key == "themes") {
-                        activity?.let { setTheme(it) }
+                    when (key) {
+                        "themes" -> activity?.let { setTheme(it) }
+                        "notifications" -> activity?.let { setNotifications(it) }
                     }
+
                 }
         prefs.registerOnSharedPreferenceChangeListener(listener)
 
